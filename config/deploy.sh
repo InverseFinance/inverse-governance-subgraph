68fd7cec98a8072539110a97feea3d53636147d9
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-11 14:35:43 +0200

    improved deploy script

--- config/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail -x
-
-npx graph-compiler --config config/mainnet.json --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
-npx graph codegen                                               ./generated/mainnet.subgraph.yaml
-npx graph deploy --product hosted-service amxx/inverse-protocol ./generated/mainnet.subgraph.yaml
