6dc258a3d213e076dbc3e3046dbfb09ebdc09dd5
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-06-17 22:43:30 +0200

    track ForceReplenish events

--- src/datasources/dbr.ts
+++ src/datasources/dbr.ts
@@ -2,13 +2,15 @@ import { Address } from '@graphprotocol/graph-ts'
 
 import {
 	ERC20Balance,
+	ForceReplenish,
 	NewMarket,
 } from '../../generated/schema'
 
 import {
-	DBR       as DBRContract,
-	AddMarket as AddMarketEvent,
-	Transfer  as TransferEvent,
+	DBR            as DBRContract,
+	AddMarket      as AddMarketEvent,
+	ForceReplenish as ForceReplenishEvent,
+	Transfer       as TransferEvent,
 } from '../../generated/dbr/DBR'
 
 import {
@@ -30,19 +32,39 @@ import {
 	fetchMarket
 } from '../fetch/market'
 
-
 export function handleAddMarket(event: AddMarketEvent): void {
-	let market = fetchMarket(event.params.market);
-
 	let ev         = new NewMarket(events.id(event))
 	ev.emitter     = fetchAccount(event.address).id
 	ev.transaction = transactions.log(event).id
 	ev.timestamp   = event.block.timestamp
-	ev.market      = market.id
+	ev.market      = fetchMarket(event.params.market).id
 	ev.save()
 }
 
-function update(token: Address, user: Address): void {
+export function handleForceReplenish(event: ForceReplenishEvent): void {
+	let ev               = new ForceReplenish(events.id(event))
+	ev.emitter           = fetchAccount(event.address).id
+	ev.transaction       = transactions.log(event).id
+	ev.timestamp         = event.block.timestamp
+	ev.market            = fetchMarket(event.params.market).id
+	ev.account           = fetchAccount(event.params.account).id
+	ev.replenisher       = fetchAccount(event.params.replenisher).id
+	ev.replenisherReward = event.params.replenisherReward
+	ev.replenishmentCost = event.params.replenishmentCost
+	ev.deficit           = event.params.deficit
+	ev.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+	if (event.params.from != constants.ADDRESS_ZERO) {
+		onTransferUpdate(event.address, event.params.from)
+	}
+	if (event.params.to != constants.ADDRESS_ZERO) {
+		onTransferUpdate(event.address, event.params.to)
+	}
+}
+
+function onTransferUpdate(token: Address, user: Address): void {
 	let instance                  = DBRContract.bind(token)
 	let dueTokensAccrued          = instance.try_dueTokensAccrued(user)
 	let deficitOf                 = instance.try_deficitOf(user)
@@ -54,13 +76,4 @@ function update(token: Address, user: Address): void {
 	balance.deficitExact          = deficitOf.reverted        ? null : deficitOf.value
 	balance.deficit               = deficitOf.reverted        ? null : decimals.toDecimals(deficitOf.value, contract.decimals)
 	balance.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-	if (event.params.from != constants.ADDRESS_ZERO) {
-		update(event.address, event.params.from)
-	}
-	if (event.params.to != constants.ADDRESS_ZERO) {
-		update(event.address, event.params.to)
-	}
 }
