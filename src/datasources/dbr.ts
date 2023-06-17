06c271b1d12bd703359e97c853a8a29758ec7ef4
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-06-17 18:10:13 +0200

    track decifit and dueToken

--- src/datasources/dbr.ts
+++ src/datasources/dbr.ts
@@ -1,14 +1,19 @@
+import { Address } from '@graphprotocol/graph-ts'
+
 import {
-	Market,
+	ERC20Balance,
 	NewMarket,
 } from '../../generated/schema'
 
 import {
+	DBR       as DBRContract,
 	AddMarket as AddMarketEvent,
+	Transfer  as TransferEvent,
 } from '../../generated/dbr/DBR'
 
-
 import {
+	constants,
+	decimals,
 	events,
 	transactions,
 } from '@amxx/graphprotocol-utils'
@@ -18,8 +23,13 @@ import {
 } from '@openzeppelin/subgraphs/src/fetch/account'
 
 import {
+	fetchERC20,
+} from '@openzeppelin/subgraphs/src/fetch/erc20'
+
+import {
 	fetchMarket
 } from '../fetch/market'
+
 
 export function handleAddMarket(event: AddMarketEvent): void {
 	let market = fetchMarket(event.params.market);
@@ -30,4 +40,27 @@ export function handleAddMarket(event: AddMarketEvent): void {
 	ev.timestamp   = event.block.timestamp
 	ev.market      = market.id
 	ev.save()
+}
+
+function update(token: Address, user: Address): void {
+	let instance                  = DBRContract.bind(token)
+	let dueTokensAccrued          = instance.try_dueTokensAccrued(user)
+	let deficitOf                 = instance.try_deficitOf(user)
+
+	let contract                  = fetchERC20(token);
+	let balance                   = ERC20Balance.load(token.toHex().concat('/').concat(user.toHex()))!
+	balance.dueTokensAccruedExact = dueTokensAccrued.reverted ? null : dueTokensAccrued.value
+	balance.dueTokensAccrued      = dueTokensAccrued.reverted ? null : decimals.toDecimals(dueTokensAccrued.value, contract.decimals)
+	balance.deficitExact          = deficitOf.reverted        ? null : deficitOf.value
+	balance.deficit               = deficitOf.reverted        ? null : decimals.toDecimals(deficitOf.value, contract.decimals)
+	balance.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+	if (event.params.from != constants.ADDRESS_ZERO) {
+		update(event.address, event.params.from)
+	}
+	if (event.params.to != constants.ADDRESS_ZERO) {
+		update(event.address, event.params.to)
+	}
 }
